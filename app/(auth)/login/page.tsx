e2004ae256fce1fcca86acded577b659b1a14a50
reza_rahem <reza_rahem224@gmail.com>
2024-02-14 23:04:57 +0330

    done

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,6 +2,7 @@ import LoginForm from '@/components/auth/login-form';
 import Container from '@/components/ui/container';
 import { authOptions } from '@/libs/next-auth';
 import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 const LoginPage = async () => {
@@ -13,6 +14,11 @@ const LoginPage = async () => {
     <div className='mt-20'>
       <Container>
         <LoginForm />
+        <div className='mt-8'>
+          <Link href='/register' className='hover:text-blue-600'>
+            <p className='text-center'>برای عضویت کلیک کنید</p>
+          </Link>
+        </div>
       </Container>
     </div>
   );
